28c7807b269b002eb94bd48667504c0ed01fd85a
th148 <th148@users.noreply.github.com>
2021-03-14 00:13:47 -0800

    Renewed the ui file to an improved version,  have begun experimenting with pydub.  May not use pydub but we will see,  having trouble getting buttons to work properly  at present.

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import sys
+# import os
 from PySide2.QtUiTools import QUiLoader
 from PySide2.QtWidgets import QApplication, QPushButton
 from PySide2.QtCore import QFile, QObject
+
 from playsound import playsound
 
-playsound('StudioRhodesCsharp4R.wav')
+# from pydub import AudioSegment
+# from pydub.playback import play
+
+# cwd = os.getcwd()
+
+# wavepath = cwd+'\\StudioRhodesCsharp4R.wav'
+
+# sound = AudioSegment.from_file(wavepath, format="wav")
+
+# play(sound)
+# print(sound.frame_rate)
+
+# octaves = -0.5
+
+# new_sample_rate = int(sound.frame_rate * (2.0 ** octaves))
+
+# adjusted_sound = sound._spawn(sound.raw_data, overrides={'frame_rate': new_sample_rate})
+
+# play(adjusted_sound)
 
 class MainWindow(QObject):
 
@@ -19,17 +39,18 @@ class MainWindow(QObject):
 
         ui_file.close()
 
+        # self.addEventListeners()
+
         self.window.show()
 
     def addEventListeners(self):
 
-        A_button = self.window.FindChild(QPushButton, 'A_push')
-        A_button.held.connect(self.AKeyHeld)
+        self.window.findChild(QPushButton, 'A_push').clicked.connect(playsound('StudioRhodesCsharp4R.wav'))
 
-    def AKeyHeld(self, obj):
-        AKey = self.window.findChild(QPushButton, 'A_push')
+    def AKeyHeld(self):
+        self.playsound('StudioRhodesCsharp4R.wav')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    main_window = MainWindow('mini_piano.ui')
+    main_window = MainWindow('mini_piano2.ui')
     sys.exit(app.exec_()) 
